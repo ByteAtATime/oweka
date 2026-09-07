a8eb2d30a4983bf422ff6939874b5401df70ada0
ByteAtATime <byteatatime@proton.me>
2026-09-06 19:53:41 -0700

    feat: initialize ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> io::Result<()> {
+    ratatui::run(app)
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
